d6594668dbb002b76837cab08a301a95c5dfd2fd
KPMG-Mike <92472570+KPMG-Mike@users.noreply.github.com>
2021-10-19 14:02:07 -0400

    Create homework.js

---
+++ js/homework.js
@@ -0,0 +1,20 @@
+const kidQuotes = [
+  { text: "Are we there yet?", author: "anonymous"},
+  { text: "But I'm not tired!", author: "anonymous"},
+  { text: "I don't have anything to do!", author: "anonymous"},
+  { text: "I just started this show.", author: "anonymous"},
+  { text: "Can I just finish this level?", author: "anonymous"},
+  { text: "I can't like this!", author: "anonymous"},
+  { text: "But I don't have a job!", author: "anonymous"},
+  { text: "But I can't drive!", author: "anonymous"},
+  { text: "What's for dinner?", author: "anonymous"},
+  { text: "Can I have pancakes and syrup?", author: "anonymous"},
+  { text: "I just want to dance!", author: "anonymous"},
+  { text: "It won't let me save!", author: "anonymous"}
+];
+
+function getNextQuote(quotesSrc) {
+  const quote = quotesSrc[getRndInt(1, quotesSrc.length + 1) - 1];
+  const quoteElem = document.getElementById('quote');
+  quoteElem.innerHTML = quote.text;
+}
